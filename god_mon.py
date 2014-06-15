a5250acc5969df2e3e5f1ba3bd07b7f8b66edbe9
Alex Serebryakov <alex.serebr@gmail.com>
2014-06-15 23:40:03 +0300

    main and status window are separated from Monitor class

--- god_mon.py
+++ god_mon.py
@@ -12,6 +12,43 @@ class Colors:
     ATTENTION       = 4
 
 
+class StatusWindow:
+    def __init__(self, base_window):
+        (height, width) = base_window.getmaxyx()
+        self._window = base_window.subwin(20, 20, 1, 1)
+        self._window.box()
+
+    @property
+    def window(self):
+        return self._window
+
+    def update(self, state):
+        self._window.addstr(0, 7, 'State')
+        self._window.addstr(1, 1, '{0}'.format(state['name']), curses.color_pair(Colors.STANDART))
+        self._window.addstr(2, 1, 'HP {0}'.format(state['health']), curses.color_pair(Colors.HEALTH_POINTS))
+        self._window.addstr(3, 1, 'Power {0}'.format(state['godpower']), curses.color_pair(Colors.POWER_POINTS))
+        self._window.addstr(4, 1, 'Inventory items {0}'.format(len(state['inventory'])), curses.color_pair(Colors.STANDART))
+
+
+class MainWindow:
+    def __init__(self, stdscr):
+        (stdscr_height, stdscr_width) = stdscr.getmaxyx()
+        self._window = curses.newwin(stdscr_height, stdscr_width)
+        self._window.box()
+
+        self._subwindows = []
+        self._subwindows.append(StatusWindow(self.window))
+
+    @property
+    def window(self):
+        return self._window
+
+    def update(self, state):
+        for window in self._subwindows:
+            window.update(state)
+
+        self.window.refresh()
+
 class Monitor:
     def __init__(self, args):
         self.init_windows()
@@ -32,26 +69,13 @@ class Monitor:
 
     def init_windows(self):
         self.stdscr = curses.initscr()
-        (self.stdscr_height, self.stdscr_width) = self.stdscr.getmaxyx()
         self.stdscr.clear()
-
-        self.main_window = curses.newwin(self.stdscr_height, self.stdscr_width)
-        self.main_window.box()
-        (height, width) = self.main_window.getmaxyx()
-        self.status_window = self.main_window.subwin(20, 20, 1, 1)
-        self.status_window.box()
+        self.main_window = MainWindow(self.stdscr)
 
     def init_colors(self):
         curses.init_pair(Colors.STANDART, curses.COLOR_WHITE, curses.COLOR_BLACK)
         curses.init_pair(Colors.HEALTH_POINTS, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.init_pair(Colors.POWER_POINTS, curses.COLOR_BLUE, curses.COLOR_BLACK)
-
-    def update_state_window(self, state):
-        self.status_window.addstr(0, 7, 'State')
-        self.status_window.addstr(1, 1, '{0}'.format(state['name']), curses.color_pair(Colors.STANDART))
-        self.status_window.addstr(2, 1, 'HP {0}'.format(state['health']), curses.color_pair(Colors.HEALTH_POINTS))
-        self.status_window.addstr(3, 1, 'Power {0}'.format(state['godpower']), curses.color_pair(Colors.POWER_POINTS))
-        self.status_window.addstr(4, 1, 'Inventory items {0}'.format(len(state['inventory'])), curses.color_pair(Colors.STANDART))
 
     def read_state(self):
         state = None
@@ -83,8 +107,7 @@ class Monitor:
 
         while(True):
             state = json.loads(self.read_state())
-            self.update_state_window(state)
-            self.main_window.refresh()
+            self.main_window.update(state)
             time.sleep(10)
 
 def main():
