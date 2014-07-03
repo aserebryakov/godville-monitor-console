4afeab1279744c2fa2500faf38cba66cd7ac8463
Alexander Serebryakov <alex.serebr@gmail.com>
2014-07-03 17:49:09 +0300

    Unit test draft fixed

--- unit_tests/key_handler_test.py
+++ unit_tests/key_handler_test.py
@@ -1,8 +1,13 @@
+import os
+import sys
 import unittest
-from key_handler import KeyHandlingManager
+sys.path.append(os.getcwd())
+from core import KeyHandlingManager
 
 class KeyHandlingManagerTestFixture(unittest.TestCase):
-    self.manager = KeyHandlingManager()
+    def __init__(self, name):
+        super(KeyHandlingManagerTestFixture, self).__init__(name)
+        self.manager = KeyHandlingManager()
 
     def key_function1(self):
         return 1
@@ -13,15 +18,32 @@ class KeyHandlingManagerTestFixture(unittest.TestCase):
     def key_function3(self):
         return 3
 
-class HandlerRegisteringTest(KeyHandlingManager):
-    def runTest(self):
+
+class HandlerRegisteringTest(KeyHandlingManagerTestFixture):
+    def positive_test(self):
         self.manager.register_handler(1, self.key_function1)
         self.manager.register_handler(2, self.key_function2)
         self.manager.register_handler(3, self.key_function3)
-        self.assertEqual(1, self.handle_keys([1])
-        self.assertEqual(2, self.handle_keys([2])
-        self.assertEqual(3, self.handle_keys([3])
 
-class HandlerRegisteringErrorTest(KeyHandlingManager):
-    def runTest(self):
+        self.assertEqual(1, self.manager.handle_keys([1]))
+        self.assertEqual(2, self.manager.handle_keys([2]))
+        self.assertEqual(3, self.manager.handle_keys([3]))
+
+    def negative_test_already_registered(self):
         pass
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(HandlerRegisteringTest('positive_test'))
+    suite.addTest(HandlerRegisteringTest('negative_test_already_registered'))
+    return suite
+
+
+def run_all():
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+
+if __name__ == '__main__':
+    run_all()
