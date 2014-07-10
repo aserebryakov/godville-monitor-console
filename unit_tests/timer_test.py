258ab66e1cb300f88cf9376a24ec368d21fce209
Alexander Serebryakov <alex.serebr@gmail.com>
2014-07-10 18:26:20 +0300

    Unit test added for timer

---
+++ unit_tests/timer_test.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import time
+import unittest
+sys.path.append(os.getcwd())
+from core import Timer
+
+class TimerTest(unittest.TestCase):
+    def timer_test(self):
+        interval = 10
+        timer = Timer(interval)
+        for i in range(0, 10):
+            time.sleep(interval/2)
+            print('Checking')
+            if timer.expired():
+                print('Expired')
+                timer.reset()
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TimerTest('timer_test'))
+    return suite
+
+
+def run_all():
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+
+if __name__ == '__main__':
+    run_all()
