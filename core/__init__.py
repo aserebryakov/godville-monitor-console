e04ebd68fb016de06d32fa746f5e15895f16aea4
Alex Serebryakov <alex.serebr@gmail.com>
2014-07-15 08:12:12 +0300

    Added WarningWindow class

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 from .timer import Timer
 from .tiled_window import TiledWindow
+from .warning_window import WarningWindow
 
 from .text_entry import TextEntry
 from .text_entry import Colors
---
+++ core/warning_window.py
@@ -0,0 +1,38 @@
+import logging
+import curses
+from .text_entry import TextEntry
+from .monitor_window import MonitorWindowBase
+
+class WarningWindow(MonitorWindowBase):
+    def __init__(self,
+                 parent_window,
+                 text):
+
+        self._text = text
+
+        # Include borders to window size
+        width  = len(text) + 2
+        height = 5
+
+        (max_y, max_x) = parent_window.getmaxyx()
+
+        y = int((max_y - height)/2)
+        x = int((max_x - width)/2)
+
+        if (x < 0 or y < 0):
+            logging.error('%s: Warning text is too long \'%s\'',
+                          self.__init__.__name__,
+                          self._text)
+            x = 0
+            y = 0
+
+        super(WarningWindow, self).__init__('Warning',
+                                            height,
+                                            width,
+                                            parent_window,
+                                            y,
+                                            x)
+
+    def init_text_entries(self):
+        self.text_entries.append(TextEntry(self._text, '', self.width))
+        self.text_entries.append(TextEntry('Press any key...', '', self.width))
