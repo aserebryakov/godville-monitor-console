907bd622ca9cd46c860df09e68413e4bad5ed3d8
Alex Serebryakov <alex.serebr@gmail.com>
2014-07-19 19:05:34 +0300

    Rule class updated

--- monitor/status_processing/rule.py
+++ monitor/status_processing/rule.py
@@ -1,12 +1,18 @@
+import logging
+
 class Rule:
     '''
     Class describing how to process dictinary item
     '''
+
+    __allowed_conditions = ['<', '<=', '==', '!=', '>=', '>']
+
     def __init__(self, key, condition, ethalon, message):
-        self._key       = key
-        self._condition = condition
-        self._ethanlon  = ethanlon
-        self._message   = message
+        self._key         = key
+        self._condition   = condition
+        self._ethalon     = ethalon
+        self._message     = message
+        self._last_result = False
 
     @property
     def key(self):
@@ -28,10 +34,33 @@ class Rule:
         string = 'key = {0}, condition = {1}, ethalon = {2}, message = {3}'.\
                  format(self.key, self.condition, ethalon, self.message)
 
-
     def check(self, value):
         '''
-        Checks if condition is satisfied
+        Checks if condition is satisfied and last result was False
         '''
-        pass
+        if ((type(value) is not int) and (type(value) is not bool)):
+            logging.error('%s: incorrect value type %s',
+                          self.check.__name__,
+                          str(type(value)))
+            return False
+
+        if ((type(self.ethalon) is not int) and (type(self.ethalon) is not bool)):
+            logging.error('%s: incorrect ethalon type %s',
+                          self.check.__name__,
+                          str(type(self.ethalon)))
+            return False
+
+        if self.condition not in self.__allowed_conditions:
+            logging.error('%s: incorrect condition %s',
+                          self.check.__name__,
+                          str(type(self.condition)))
+            return False
+
+        result = eval(str(value) + str(self.condition) + str(self.ethalon))
+
+        if self._last_result != result:
+            self._last_result = result
+            return result
+
+        return False
 
