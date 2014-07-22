acca9009c7e3dd62ff77f2c1b372d2fbd388e2ff
Alexander Serebryakov <alex.serebr@gmail.com>
2014-07-22 17:56:28 +0300

    Dictionary checker fixed, unit test added.

--- unit_tests/dictionary_checker_test.py
+++ unit_tests/dictionary_checker_test.py
@@ -9,25 +9,59 @@ from monitor import DictionaryChecker
 class DictionaryCheckerTestFixture(unittest.TestCase):
     def setUp(self):
         self.checker = DictionaryChecker()
-        rules = {'0': 'This is first rule',
-                 '1': 'This is the second rule',
-                 '2': 'This is the third rule'}
+        self.rules = {'0': 'This is first rule',
+                      '1': 'This is the second rule',
+                      '2': 'This is the third rule'}
 
-        self.checker.add_rule(Rule('0', '<', 20, rules['0']))
-        self.checker.add_rule(Rule('1', '==', 20, rules['1']))
-        self.checker.add_rule(Rule('2', '==', False, rules['2']))
+        self.checker.add_rule(Rule('0', '<', 20, self.rules['0']))
+        self.checker.add_rule(Rule('1', '==', 20, self.rules['1']))
+        self.checker.add_rule(Rule('2', '==', False, self.rules['2']))
 
 
 class DictionaryCheckerTest(DictionaryCheckerTestFixture):
     def single_rule_test(self):
-        dictionary = {'1' : 400,
-                      '2' : 20,
-                      '3' : True}
+        dictionary = {'0' : 400,
+                      '1' : 20,
+                      '2' : True}
 
+        messages = self.checker.check_rules(dictionary)
+        self.assertEqual(1, len(messages))
+        self.assert_(self.rules['1'] == messages[0])
+
+    def multiple_rule_test(self):
+        dictionary = {'0' : 19,
+                      '1' : 20,
+                      '2' : True}
+
+        messages = self.checker.check_rules(dictionary)
+        self.assertEqual(2, len(messages))
+        self.assert_(self.rules['0'] == messages[0])
+        self.assert_(self.rules['1'] == messages[1])
+
+    def incorrect_type_test(self):
+        dictionary = {'0' : 19,
+                      '1' : 'asd',
+                      '2' : True}
+
+        messages = self.checker.check_rules(dictionary)
+        self.assertEqual(1, len(messages))
+        self.assert_(self.rules['0'] == messages[0])
+
+    def incorrect_comparison_test(self):
+        dictionary = {'0' : 19,
+                      '1' : False,
+                      '2' : True}
+
+        messages = self.checker.check_rules(dictionary)
+        self.assertEqual(1, len(messages))
+        self.assert_(self.rules['0'] == messages[0])
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(DictionaryCheckerTest('single_rule_test'))
+    suite.addTest(DictionaryCheckerTest('multiple_rule_test'))
+    suite.addTest(DictionaryCheckerTest('incorrect_type_test'))
+    suite.addTest(DictionaryCheckerTest('incorrect_comparison_test'))
     return suite
 
 def run_all():
