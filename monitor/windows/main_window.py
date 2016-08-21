9195d3419e26f1f39e87d4cee2599fb1f6370703
Dixie Flatline <umi0451@gmail.com>
2016-08-21 14:58:59 +0300

    Shuffled windows a bit, squeezed entries, added some new stats to display.

--- monitor/windows/main_window.py
+++ monitor/windows/main_window.py
@@ -10,44 +10,54 @@ class MainWindow(MonitorWindowBase):
 
         self._subwindows = []
 
-        statusWindow = MonitorWindowBase(self.window, 'Status', 0, 0, 22, 10)
-        statusWindow.add_text_entry('', 'name')
-        statusWindow.add_text_entry('HP', 'health', color=Colors.HEALTH_POINTS)
-        statusWindow.add_text_entry('Max HP', 'max_health', color=Colors.HEALTH_POINTS)
-        statusWindow.add_text_entry('Power, %', 'godpower', color=Colors.POWER_POINTS)
-        statusWindow.add_text_entry('EXP, %', 'exp_progress')
-        statusWindow.add_text_entry('Town', 'town_name')
-        statusWindow.add_text_entry('Distance', 'distance')
-        self._subwindows.append(statusWindow)
+        # Column 1: Main hero stats.
+        wnd = MonitorWindowBase(self.window, 'Session', 0, 0, 22, 3)
+        wnd.add_text_entry('', lambda state: 'Expired' if 'expired' in state else 'Active')
+        self._subwindows.append(wnd)
 
-        questWindow = MonitorWindowBase(self.window, 'Quest', 22, 0, None, 8)
-        questWindow.add_text_entry('', 'quest')
-        questWindow.add_text_entry('Progress, %', 'quest_progress')
-        questWindow.add_text_entry('', '')
-        questWindow.add_text_entry('', 'diary_last')
-        self._subwindows.append(questWindow)
+        wnd = MonitorWindowBase(self.window, 'God', 0, 3, 22, 4)
+        wnd.add_text_entry('', 'godname')
+        wnd.add_text_entry('Power:', 'godpower', color=Colors.POWER_POINTS)
+        self._subwindows.append(wnd)
 
-        petWindow = MonitorWindowBase(self.window, 'Pet', 0, 10, 22, 6)
-        petWindow.add_text_entry('', lambda state: state['pet']['pet_class'])
-        petWindow.add_text_entry('', lambda state: state['pet']['pet_name'])
-        petWindow.add_text_entry('Level', lambda state: state['pet']['pet_level'])
-        self._subwindows.append(petWindow)
+        wnd = MonitorWindowBase(self.window, 'Hero', 0, 7, 22, 12)
+        wnd.add_text_entry('', 'name')
+        wnd.add_text_entry('', 'alignment')
+        wnd.add_text_entry('HP:', lambda state: '{0}/{1}'.format(state['health'], state['max_health']), color=Colors.HEALTH_POINTS)
+        wnd.add_text_entry('Lvl:', lambda state: '{0} ({1}%)'.format(state['level'], state['exp_progress']))
+        wnd.add_text_entry('Clan:', lambda state: '{0}, {1}'.format(state['clan'], state['clan_position']))
+        wnd.add_text_entry('Temple:', lambda state: 'done' if state['temple_completed_at'] else str(state['bricks_cnt']))
+        wnd.add_text_entry('Ark:', lambda state: 'done' if state['ark_completed_at'] else str(state['wood_cnt']))
+        wnd.add_text_entry('Location:', lambda state: 'Fight!' if state['arena_fight'] else ( state['town_name'] if state['town_name'] else '{0} pl'.format(state['distance']) ) )
+        self._subwindows.append(wnd)
 
-        inventoryWindow = MonitorWindowBase(self.window, 'Inventory', 22, 8, None, 8)
-        inventoryWindow.add_text_entry('Gold', 'gold_approx')
-        inventoryWindow.add_text_entry('Bricks', 'bricks_cnt')
-        inventoryWindow.add_text_entry('Wood', 'wood_cnt')
-        inventoryWindow.add_text_entry('Useful Items',
+        wnd = MonitorWindowBase(self.window, 'Pet', 0, 19, 22, 5)
+        wnd.add_text_entry('', lambda state: '{0} {1}'.format(state['pet']['pet_class'], state['pet']['pet_name']))
+        wnd.add_text_entry('Level:', lambda state: state['pet']['pet_level'])
+        self._subwindows.append(wnd)
+
+        if height > 24:
+            wnd = MonitorWindowBase(self.window, '', 0, 24, 22, None)
+            self._subwindows.append(wnd)
+
+        # Column 2: Inventory.
+        wnd = MonitorWindowBase(self.window, 'Inventory', 22, 0, 30, None)
+        wnd.add_text_entry('Gold:', 'gold_approx')
+        wnd.add_text_entry('Items:', lambda state: '{0}/{1}'.format(state['inventory_num'], state['inventory_max_num']))
+        wnd.add_text_entry('Useful Items',
             lambda state: sum([(1 if 'activate_by_user' in item else 0) for item in state['inventory'].values()]))
-        inventoryWindow.add_text_entry('High Cost Items',
+        wnd.add_text_entry('High Cost Items',
             lambda state: sum([(1 if item['price'] > 0 else 0) for item in state['inventory'].values()]))
-        inventoryWindow.add_text_entry('Total Items', 'inventory_num')
-        self._subwindows.append(inventoryWindow)
+        self._subwindows.append(wnd)
 
-        applicationStatusWindow = MonitorWindowBase(self.window, 'Session', 0, 16, 22, 3)
-        applicationStatusWindow.add_text_entry('',
-            lambda state: 'Expired' if 'expired' in state else 'Active')
-        self._subwindows.append(applicationStatusWindow)
+        # Column 3: Quest, diary etc.
+        wnd = MonitorWindowBase(self.window, 'Log', 52, 0, None, None)
+        wnd.add_text_entry('Quest: ', lambda state: '{0} ({1}%)'.format(state['quest'], state['quest_progress']))
+        wnd.add_text_entry('', '')
+        wnd.add_text_entry('', lambda state: '"{0}"'.format(state['motto']))
+        wnd.add_text_entry('', lambda x: '* * *')
+        wnd.add_text_entry('- ', 'diary_last')
+        self._subwindows.append(wnd)
 
     def update(self, state):
         super(MainWindow, self).update(state)
